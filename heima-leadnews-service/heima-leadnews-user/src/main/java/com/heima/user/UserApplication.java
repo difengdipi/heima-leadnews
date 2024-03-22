ef3ef90348a56d46d3b0363af684d50c1729ea3a
周海 <2358853434@qq.com>
2024-03-22 10:31:54 +0800

    用户app端登录功能

---
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
@@ -0,0 +1,40 @@
+package com.heima.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.heima.user.mapper")
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+        System.out.println("////////////////////////////////////////////////////////////////////\n" +
+                "//                          _ooOoo_                               //\n" +
+                "//                         o8888888o                              //\n" +
+                "//                         88\" . \"88                              //\n" +
+                "//                         (| ^_^ |)                              //\n" +
+                "//                         O\\  =  /O                              //\n" +
+                "//                      ____/`---'\\____                           //\n" +
+                "//                    .'  \\\\|     |//  `.                         //\n" +
+                "//                   /  \\\\|||  :  |||//  \\                        //\n" +
+                "//                  /  _||||| -:- |||||-  \\                       //\n" +
+                "//                  |   | \\\\\\  -  /// |   |                       //\n" +
+                "//                  | \\_|  ''\\---/''  |   |                       //\n" +
+                "//                  \\  .-\\__  `-`  ___/-. /                       //\n" +
+                "//                ___`. .'  /--.--\\  `. . ___                     //\n" +
+                "//              .\"\" '<  `.___\\_<|>_/___.'  >'\"\".                  //\n" +
+                "//            | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |                 //\n" +
+                "//            \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /                 //\n" +
+                "//      ========`-.____`-.___\\_____/___.-`____.-'========         //\n" +
+                "//                           `=---='                              //\n" +
+                "//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //\n" +
+                "//             佛祖保佑       永不宕机      永无BUG                   //\n" +
+                "////////////////////////////////////////////////////////////////////"+"\n" +
+                "leadnews-user-service启动成功");
+    }
+}
